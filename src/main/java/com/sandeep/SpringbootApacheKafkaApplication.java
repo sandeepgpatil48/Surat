e7d3de368c4f74cea89aa2caf4b4b93163321694
sandeep <sandeeppatil776@gmail.com>
2022-12-29 12:23:10 +0530

    Rohit Commited successfully

---
+++ src/main/java/com/sandeep/SpringbootApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApacheKafkaApplication.class, args);
+	}
+
+}
